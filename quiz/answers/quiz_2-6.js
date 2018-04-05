d2b57e1c2b6a1e792d61d4bee5d4b20fae28f035
JavaScript CBT User <javascript@company.pri>
2018-04-05 12:50:31 +0200

    Add quiz_2-6.js & answer

---
+++ quiz/answers/quiz_2-6.js
@@ -0,0 +1,13 @@
+/*
+ * Programming Quiz: Yosa Buson (2-6)
+ */
+
+ /* Build a string using concatenation by combining the lines from this famous haiku poem by Yosa Buson.
+
+  * Blowing from the west
+  * Fallen leaves gather
+  * In the east.
+*/
+
+var haiku = 'Blowing from the west\n' + 'Fallen leaves gather\n' + 'In the east.'
+console.log(haiku);
---
+++ quiz/quiz_2-6.js
@@ -0,0 +1,13 @@
+/*
+ * Programming Quiz: Yosa Buson (2-6)
+ */
+
+ /* Build a string using concatenation by combining the lines from this famous haiku poem by Yosa Buson.
+
+  * Blowing from the west
+  * Fallen leaves gather
+  * In the east.
+*/
+
+var haiku = /* concatenate the strings here */
+console.log(haiku);
