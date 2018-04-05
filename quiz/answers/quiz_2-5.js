5ff791de8e4fc6ecb3e8c7d49399d8a3e3cb5efd
JavaScript CBT User <javascript@company.pri>
2018-04-05 12:44:27 +0200

    Fixed typo in quiz/answers/quiz_2-5.js

--- quiz/answers/quiz_2-5.js
+++ quiz/answers/quiz_2-5.js
@@ -2,5 +2,5 @@
  * Programming Quiz: All Tied Up (2-5)
  */
 
-var joke = 'Do you know why I live in constant fear?\nI threw a boomerang 6years ago and it never came back!'
+var joke = 'Do you know why I live in constant fear?\nI threw a boomerang 6 years ago and it never came back!'
 console.log(joke);
