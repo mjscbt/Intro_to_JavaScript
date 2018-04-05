9e6ebf4b4e37c6795235eb976e372e6473833bc1
JavaScript CBT User <javascript@company.pri>
2018-04-05 14:15:37 +0200

    Add quiz_2-12.js & answer

---
+++ quiz/quiz_2-12.js
@@ -0,0 +1,23 @@
+/*
+ * Programming Quiz: One Awesome Message (2-12)
+ *
+ * 1. Create the variables:
+ *     - firstName
+ *     - interest
+ *     - hobby
+ *
+ * 2. Create a variable named awesomeMessage and, using string concatenation
+ *    and the variables above, create an awesome message inf the form
+ *    "Hi, my name is Julia. I love cats. In my spare time, I like to play video games."
+ *
+ * 3. Print the awesomeMessage variable to the console.
+ */
+
+/*
+ * Notes:
+ * - Using the above as an example, firstName would have been assigned to
+ *   "Julia", interest to "cats", and hobby to "play video games".
+ * - Be sure to include spaces and periods where necessary!
+ */
+
+// Add your code here
