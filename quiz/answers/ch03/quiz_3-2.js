aa2da2e73c2392b2e5b51866ce6cdac71d00fb60
JavaScript CBT User <javascript@company.pri>
2018-04-10 09:36:25 +0200

    Add quiz_3-2.js, quiz_3-3.js & answers

--- quiz/answers/ch03/quiz_3-2.js
+++ quiz/answers/ch03/quiz_3-2.js
@@ -5,13 +5,18 @@
  * number is even and prints `odd` if the number is odd.
  *
  * Note - make sure to print only the string "even" or the string "odd"
+ *
+ * Hint: Use the % (modulo) operator to determine if a number is even or odd.
+ * The modulo operater takes two numbers and returns the remainder when the
+ * first number is divided by the second one:
+ *
  */
 
 // change the value of `number` to test your if...else statement
 var number = 2;
 
-if (/* your conditional goes here */) {
-
+if (number % 2 === 0) {
+  console.log('even')
 } else {
-
+  console.log('odd')
 }
--- quiz/ch03/quiz_3-2.js
+++ quiz/ch03/quiz_3-2.js
@@ -5,6 +5,11 @@
  * number is even and prints `odd` if the number is odd.
  *
  * Note - make sure to print only the string "even" or the string "odd"
+ *
+ * Hint: Use the % (modulo) operator to determine if a number is even or odd.
+ * The modulo operater takes two numbers and returns the remainder when the
+ * first number is divided by the second one:
+ *
  */
 
 // change the value of `number` to test your if...else statement
