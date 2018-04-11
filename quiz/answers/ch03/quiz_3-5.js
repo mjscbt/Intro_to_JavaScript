c76f354cf587d663bf7a61b2aed23b002b4faa00
JavaScript CBT User <javascript@company.pri>
2018-04-11 12:00:53 +0200

    Add quiz_3-5.js, quiz_3-5-flowchart.jpg & answer

---
+++ quiz/answers/ch03/quiz_3-5.js
@@ -0,0 +1,37 @@
+/*
+ * Programming Quiz - Checking Your Balance (3-5)
+ *
+ * Using the flowchart quiz_3-5-flowchart.jpg, write the code to represent
+ * checking your balance at the ATM. The yellow diamonds represent conditional
+ * statements and the blue rectangles with rounded corners represent what
+ * should be printed to the console.
+ *
+ * Hint: The variable balance could be a value less than, greater than, or equal
+ * to 0. The variables isActive and checkBalance are booleans that can be set
+ * to true or false.
+ *
+ * TIP: To print out the account balance with decimal points (i.e. 325.00),
+ * use the .toFixed() method and pass it the number of decimal points you want
+ * to use. For example, balance.toFixed(2) returns 325.00.
+ *
+ */
+
+// change the values of `balance`, `checkBalance`, and `isActive` to test your code
+var balance = 325.00;
+var checkBalance = true;
+var isActive = false;
+
+// your code goes here
+if (checkBalance) {
+  if (isActive && balance > 0) {
+    console.log('Your balance is $' + balance.toFixed(2) + '.');
+  } else if (!isActive) {
+    console.log('Your account is no longer active.');
+  } else if (balance === 0) {
+    console.log('Your account is empty.');
+  } else {
+    console.log('Your balance is negative. Please contact bank.');
+  }
+} else {
+  console.log('Thank you. Have a nice day!');
+}
